5d0b3dd6b43c8da5d21ea532c4b7205ca1807c6e
Leonardo Banderali <leonardo2718@protonmail.com>
2018-10-20 14:26:10 -0400

    Implement expression parser

--- rust/src/parser.rs
+++ rust/src/parser.rs
@@ -40,6 +40,7 @@ use ast;
 #[derive(Debug,Clone,PartialEq)]
 pub enum ParseError {
     UnexpectedToken(token::TokenType),
+    ExpectingMoreTokens,
     LexerError(lexer::Error)
 }
 
@@ -56,6 +57,7 @@ impl error::Error for Error {
         use self::ParseError::*;
         match **self {
             UnexpectedToken(_) => "Encountered unexpected token while parsing.",
+            ExpectingMoreTokens => "Expected to find more tokens but got nothing",
             LexerError(_) => "Error occurred during lexing.",
         }
     }
@@ -77,11 +79,165 @@ impl convert::From<lexer::Error> for Error {
 
 type Result<T> = result::Result<T, Error>;
 
+#[derive(Debug,Clone,Copy,PartialEq,Eq)]
+enum Associativity {
+    Left,
+    Rigth
+}
+
+impl token::Operator {
+    fn precedence(self) -> u32 {
+        use token::Operator::*;
+        match self {
+            ADD => 2,
+            SUB => 2,
+            MUL => 3,
+            DIV => 3,
+        }
+    }
+
+    fn associativity(self) -> Associativity {
+        Associativity::Left
+    }
+}
+
+/*
+Dijkstra's shunting-yard algorithm:
+
+while there are tokens to be read:
+    read a token.
+    if the token is a number, then:
+        push it to the output queue.
+    if the token is a function then:
+        push it onto the operator stack 
+    if the token is an operator, then:
+        while ((there is a function at the top of the operator stack)
+               or (there is an operator at the top of the operator stack with greater precedence)
+               or (the operator at the top of the operator stack has equal precedence and is left associative))
+              and (the operator at the top of the operator stack is not a left bracket):
+            pop operators from the operator stack onto the output queue.
+        push it onto the operator stack.
+    if the token is a left bracket (i.e. "("), then:
+        push it onto the operator stack.
+    if the token is a right bracket (i.e. ")"), then:
+        while the operator at the top of the operator stack is not a left bracket:
+            pop the operator from the operator stack onto the output queue.
+        pop the left bracket from the stack.
+        /* if the stack runs out without finding a left bracket, then there are mismatched parentheses. */
+if there are no more tokens to read:
+    while there are still operator tokens on the stack:
+        /* if the operator token on the top of the stack is a bracket, then there are mismatched parentheses. */
+        pop the operator from the operator stack onto the output queue.
+exit.
+*/
 fn parse_expression<L: Lexer>(lexer: &mut L) -> Result<WithPos<ast::WithType<ast::Expression>>> {
     use utils::Const::*;
+    use token::TokenType::*;
     use ast::Expression::*;
     use ast::Type;
-    Ok(WithPos{item: ast::WithType{item: Literal(I32(3)), t: Type::I32}, position: Position{pos:0}})
+
+    let mut expr_stack: Vec<WithPos<ast::WithType<ast::Expression>>> = Vec::new();
+    let mut op_stack: Vec<token::Token> = Vec::new();
+
+    macro_rules! push_expr {
+        ($token:expr, $type:expr, $expr:ident, $($args:expr),* ) => {
+            expr_stack.push(WithPos{item:ast::WithType{item:$expr($($args),*), t:$type}, position:$token.pos});
+        };
+    }
+
+    macro_rules! optoken2opast {
+        ($optoken:expr) => {{
+            use token::Operator::*;
+            use ast::BinaryOperator::*;
+            match $optoken {
+                ADD => Add,
+                SUB => Sub,
+                MUL => Mul,
+                DIV => Div
+            }
+        }};
+    }
+
+    macro_rules! pop_op {
+        ($optoken:expr,$t:expr) => {
+            let rhs = expr_stack.pop().ok_or(WithPos{item:ParseError::UnexpectedToken($t.token.clone()), position:$t.pos})?;
+            let lhs = expr_stack.pop().ok_or(WithPos{item:ParseError::UnexpectedToken($t.token.clone()), position:$t.pos})?;
+            push_expr!($t, Type::Unknown, BinaryOp, optoken2opast!($optoken), Box::new(lhs), Box::new(rhs));
+        };
+    }
+
+    while let Some(next) = lexer.clone().peekable().peek() {
+        let next = next.clone()?;
+
+        match next.token {
+            IDENT(s) => { lexer.next(); push_expr!(next, Type::Unknown, Identifier, s); },
+            CONST(c) => { lexer.next(); match c {
+                I32(_) => push_expr!(next, Type::I32, Literal, c)
+            };},
+            OPERATOR(o) => {
+                lexer.next();
+                while let Some(t) = op_stack.pop() {
+                    if let OPERATOR(p) = t.token {
+                        if p.precedence() > o.precedence() { pop_op!(o,next); }
+                        else if p.precedence() == o.precedence() && p.associativity() == Associativity::Left { pop_op!(o,next); }
+                        else { break; }
+                    }
+                    else if LPAREN == t.token {
+                        op_stack.push(t);
+                        break;
+                    }
+                    else {
+                        return Err(WithPos{item:ParseError::UnexpectedToken(t.clone().token),position:t.pos})
+                    }
+                }
+                op_stack.push(next);
+            },
+            LPAREN => {
+                lexer.next();
+                op_stack.push(next);
+            },
+            RPAREN => {
+                let mut inexpr = false;
+                while let Some(t) = op_stack.pop() {
+                    if let OPERATOR(p) = t.token { pop_op!(p,t); }
+                    else if LPAREN == t.token { inexpr = true; break; }
+                    else { return Err(WithPos{item:ParseError::UnexpectedToken(t.clone().token),position:t.pos}); }
+                }
+
+                if !inexpr && expr_stack.len() == 1 { return Ok(expr_stack.pop().unwrap()); }
+                else { lexer.next(); }
+            },
+            t => {
+                break;
+            },
+        }
+    }
+
+    while let Some(t) = op_stack.pop() {
+        if let OPERATOR(p) = t.token { pop_op!(p,t); }
+        else { return Err(WithPos{item:ParseError::UnexpectedToken(t.clone().token),position:t.pos}); }
+    }
+
+    if expr_stack.len() == 1 {
+        Ok(expr_stack.pop().unwrap())
+    }
+    else {
+        Err(WithPos{item:ParseError::ExpectingMoreTokens, position: Position{pos:0}})
+    }
+}
+
+macro_rules! expect_token {
+    ($pos:expr, $lexer:expr, $token:tt) => {
+        if let Some(next) = $lexer.next() {
+            match next?.token {
+                token::TokenType::$token => Ok(()),
+                t => Err(WithPos{item: ParseError::UnexpectedToken(t), position: $pos})
+            }
+        }
+        else {
+            Err(WithPos{item: ParseError::ExpectingMoreTokens, position: $pos})
+        }
+    };
 }
 
 fn parse_statement<L: Lexer>(lexer: &mut L) -> Result<WithPos<ast::Statement>>  {
@@ -93,7 +249,9 @@ fn parse_statement<L: Lexer>(lexer: &mut L) -> Result<WithPos<ast::Statement>>  
     let token = lexer.next().unwrap()?;
     match token.token {
         KEYWORD(PRINT) => {
+            expect_token!(token.pos, lexer, LPAREN)?;
             let expr = parse_expression(lexer)?;
+            expect_token!(expr.pos(), lexer, RPAREN)?;
             return Ok(WithPos{item: Print(expr), position: token.pos});
         }
         t => Err(Error{item: ParseError::UnexpectedToken(t), position: token.pos})
@@ -109,7 +267,7 @@ fn parse_statement_list<L: Lexer>(lexer: &mut L) -> Result<ast::StatementList> {
     Ok(stmts)
 }
 
-pub fn parse_program<L: Lexer>(mut lexer: L) -> Result<ast::Program> {
+pub fn parse_program<L: Lexer>(lexer: L) -> Result<ast::Program> {
     use token::TokenType::*;
     use token::Token;
     let mut lexer = lexer.filter(|ref t| if let Ok(Token{token:COMMENT(_), pos:_}) = t { false } else { true });
